6fecbd3731a9a6a4c5a00c7bd7c2097a99a318fa
Mateusz Cieciak <mc424i@gmail.com>
2016-08-12 12:06:13 +0200

    spring configuration

--- src/main/java/com/mcieciak/config/ApplicationInitializer.java
+++ src/main/java/com/mcieciak/config/ApplicationInitializer.java
@@ -1,7 +1,34 @@
 package com.mcieciak.config;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 /**
  * Created by Mateusz on 11.08.2016.
  */
-public class ApplicationInitializer {
+
+// Tworzenie DispatcherServlet
+
+public class ApplicationInitializer implements WebApplicationInitializer{
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext applicationContext= new
+                AnnotationConfigWebApplicationContext();
+        applicationContext.register(AppConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+
+        ServletRegistration.Dynamic servletRegistration
+                =servletContext.addServlet("dispatcher", dispatcherServlet);
+        // 1 to priorytet, oznacza, ze ten servlet jest najwazniejszy
+
+        servletRegistration.setLoadOnStartup(1);
+        servletRegistration.addMapping("/");
+
+    }
 }
